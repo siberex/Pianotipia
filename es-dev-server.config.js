a68fcdbf6ad1067077aa5e2b2ab267f3c05cf6c7
Stepan Legachev <gitlog@sib.li>
2020-01-25 23:47:47 +0300

    build(dx): add es-dev-server

---
+++ es-dev-server.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    port: 8080,
+    http2: true,
+    watch: true,
+    nodeResolve: true,
+    appIndex: 'docs/index.html',
+    open: true,
+    moduleDirs: ['node_modules', 'dist'],
+    dedupe: true,
+    preserveSymlinks: true,
+};
