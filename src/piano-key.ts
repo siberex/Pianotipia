890e0d859733d1f1bc3aa26d20ddaa2ebe32ea71
Stepan Legachev <gitlog@sib.li>
2020-01-26 04:59:30 +0300

    feat(keyboard): expand PianoKey class

--- src/piano-key.ts
+++ src/piano-key.ts
@@ -10,6 +10,7 @@ export enum KeyName {C, Db, D, Eb, E, F, Gb, G, Ab, A, Bb, B}
 export interface Key {
     name: string;
     pressed: boolean;
+    standalone: boolean;
 }
 
 @customElement('piano-key')
@@ -20,6 +21,13 @@ export class PianoKey extends LitElement implements Key {
 
     @property({type: Boolean})
     pressed = false;
+
+    /**
+     * Flag to render stand-alone full-size white keys, located at the end of the keyboard.
+     * Or, in rare cases, at the start of it (like on Yamaha P-121, Casio WK-6600 or Nord Stage 3 Compact).
+     */
+    @property({type: Boolean})
+    standalone = false;
 
     render() {
         return html`<strong>${this.name}</strong>`;
